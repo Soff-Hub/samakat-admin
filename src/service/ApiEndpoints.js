a7e66cd5b473bf7e8586a9aebdb11338a1e1e099
lazywk <edoniyorbekdev@gmail.com>
2023-10-23 14:49:52 +0500

    finished branches

--- src/service/ApiEndpoints.js
+++ src/service/ApiEndpoints.js
@@ -3,5 +3,6 @@ export const API_ENDPOINTS = {
     PROFILE: 'auth/profile/',
     CREATE_BRANCH: 'admin/branch/create/',
     UPDATE_BRANCH: 'admin/branch/update/',
+    DELETE_BRANCH: 'admin/branch/delete/',
     GET_BRANCHS:'admin/branch/'
 }
