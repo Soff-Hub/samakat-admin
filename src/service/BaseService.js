22e8945b21c8c21a7139e443ed150993499d30fe
lazywk <edoniyorbekdev@gmail.com>
2023-11-04 12:46:13 +0500

    auto logout

--- src/service/BaseService.js
+++ src/service/BaseService.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 import { appConfig } from "configs/app.config"
+import store from "store"
+import { logoutSuccess } from "store/slice"
 
 
 
@@ -7,6 +9,8 @@ const BaseService = axios.create({
     baseURL: appConfig.apiPrefix,
     timeout: 10000,
 })
+
+const unAuthorizeCode = [403]
 
 
 BaseService.interceptors.request.use(
@@ -25,7 +29,13 @@ BaseService.interceptors.request.use(
 
 BaseService.interceptors.response.use(
     (response) => response.data,
-    (error) => Promise.reject(error)
+    (error) => {
+        const { response } = error
+        if (response && unAuthorizeCode.includes(response.status)) {
+            store.dispatch(logoutSuccess())
+        }
+        return Promise.reject(error)
+    }
 )
 
 export default BaseService
