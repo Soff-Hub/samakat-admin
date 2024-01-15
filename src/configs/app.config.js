a6c6b09f47999ece471decdf81c3b8de7a005b04
Farangiz17 <farangizismatova24@gmail.com>
2024-01-15 15:39:14 +0500

    replace alokand.uz

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,4 +1,4 @@
 export const appConfig = {
-    apiPrefix: 'http://192.168.1.21/api/v1'
-    // apiPrefix: 'https://api.alokand.uz/api/v1'
+    // apiPrefix: 'http://192.168.1.21/api/v1'
+    apiPrefix: 'https://api.alokand.uz/api/v1'
 }
