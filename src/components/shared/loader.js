f8e7d1e9c13efba48beba763312460c68382cdf6
Farangiz17 <farangizismatova24@gmail.com>
2024-01-18 18:52:48 +0500

    loader

--- src/components/shared/loader.js
+++ src/components/shared/loader.js
@@ -3,8 +3,8 @@ import { FadeLoader } from "react-spinners";
 
 function Loader() {
   return (
-    <div className="w-full flex justify-center  my-auto mt-[18%]">
-      <FadeLoader   color="#3B82F6" size={80} />
+    <div className=" d-flex justify-content-center align-items-center mt-5 "  >
+      <FadeLoader  color="#3B82F6" size={80} />
     </div>
   );
 }
