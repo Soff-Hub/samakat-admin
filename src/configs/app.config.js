209c84fdaaed9481fd41330218f90071c2454293
Farangiz17 <farangizismatova24@gmail.com>
2024-04-04 15:56:04 +0500

    base url

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,4 +1,4 @@
 export const appConfig = {
-    apiPrefix: 'http://192.168.1.24/api/v1'
-    // apiPrefix: 'https://api.alokand.uz/api/v1'
+    // apiPrefix: 'http://192.168.1.24/api/v1'
+    apiPrefix: 'https://api.alokand.uz/api/v1'
 }
