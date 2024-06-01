cdf8ae8e1f795e59935fafd6a5a6845b46bf77cd
Farangiz17 <farangizismatova24@gmail.com>
2024-06-01 16:59:25 +0500

    login

--- src/views/auth/login.js
+++ src/views/auth/login.js
@@ -20,12 +20,9 @@ function Login() {
   const openNotification = (placement) => {
     api.error({
       message: ` Xatolik ! `,
-      description: (<>
-     { placement ? placement : 'Qayta to\'ldiring'}
-      </>
-      )});
+      description: <>{placement ? placement : "Qayta to'ldiring"}</>,
+    });
   };
- 
 
   const handleSubmit = async (values) => {
     // console.log('value', values);
@@ -39,7 +36,7 @@ function Login() {
       })
       .catch((err) => {
         openNotification(err?.response?.data?.msg?.[0]);
-        
+
         setErrPhone(err?.response?.data?.msg?.[0]);
         setErrPass(err?.response?.data?.msg?.[0]);
         setSubmiting(false);
@@ -47,8 +44,9 @@ function Login() {
   };
 
   return (
-    <>{contextHolder}
-      <div className="login-page">
+    <>
+      {contextHolder}
+      {/* <div className="login-page">
         <Formik
           initialValues={{ phone: "", password: "" }}
           validate={(values) => {
@@ -137,6 +135,205 @@ function Login() {
                   {submiting ? "Kutilmoqda..." : "Kirish"}
                 </Button>
               </form>
+            </div>
+          )}
+        </Formik>
+      </div> */}
+      <div className="d-flex">
+        <div className="login-page xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-1/2 w-full     ">
+           <div className=" lg:w-2/4 flex xl:items-start lg:items-start md:items-start sm:items-start items-center	xl:justify-start lg:justify-start md:justify-start sm:justify-start justify-center	 p-16 ">
+                <img src={Logo} alt="aloqand logo" className="xl:w-75 lg:w-75 md:w-75 sm:w-75 w-75 " />
+              </div>
+              <Formik
+          initialValues={{ phone: "", password: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.phone || errPhone) {
+              errors.phone = errPhone || "Telefon majburiy";
+            }
+
+            if (!values.password || errPass) {
+              errors.password = "Parol majburiy";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            handleSubmit(values);
+            setSubmitting(true);
+            setSubmiting(true);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <div className="  xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-1/2 w-full xl:hidden lg:hidden md:hidden sm:hidden block">
+              <form
+                onSubmit={handleSubmit}
+                className="login-form-inner flex flex-col 
+                   xl:2/4 lg:w-2/3 sm:w-full
+                 w-full mt-28 xl:mt-28 lg:mt-28 sm:mt-8 mt-8 mx-auto
+                 
+                   px-5 gap-5"
+              >
+                <div className="login-title lg:w-2/4 font-sans text-3xl font-bold	px-2 text-center	justify-center ">
+                  Kirish
+                </div>
+                <TextField
+                  label="Telefon raqam"
+                  // eslint-disable-next-line
+                  error={
+                    (values.phone && errors.phone) ||
+                    (values.phone === "" && errors.phone) ||
+                    errPhone
+                  }
+                  helperText={errors.phone || errPhone}
+                  variant="filled"
+                  size="small"
+                  name="phone"
+                  type="phone"
+                  onChange={(e) => {
+                    handleChange(e);
+                    setErrPhone(false);
+                    setErrPass(false);
+                  }}
+                  onBlur={handleBlur}
+                  value={values.phone}
+                />
+                <TextField
+                  // eslint-disable-next-line
+                  error={
+                    (values.password && errors.password) ||
+                    errors.password ||
+                    errPass
+                  }
+                  helperText={errors.password || errPass}
+                  label="Parol"
+                  variant="filled"
+                  size="small"
+                  name="password"
+                  onChange={(e) => {
+                    handleChange(e);
+                    setErrPhone(false);
+                    setErrPass(false);
+                  }}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  type="password"
+                />
+                <Button
+                  variant="contained"
+                  size="large"
+                  type="submit"
+                  disabled={submiting}
+                  style={{backgroundColor:'#D7144C'}}
+                >
+                  {submiting ? "Kutilmoqda..." : "Kirish"}
+                </Button>
+              </form>
+             
+            </div>
+          )}
+        </Formik>
+        </div>
+        <Formik
+          initialValues={{ phone: "", password: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.phone || errPhone) {
+              errors.phone = errPhone || "Telefon majburiy";
+            }
+
+            if (!values.password || errPass) {
+              errors.password = "Parol majburiy";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            handleSubmit(values);
+            setSubmitting(true);
+            setSubmiting(true);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <div className="  w-1/2 xl:flex lg:flex md:flex sm:flex hidden">
+              <form
+                onSubmit={handleSubmit}
+                className="login-form-inner flex flex-col 
+                   xl:2/4 lg:w-2/3 sm:w-full
+                 w-full mt-28 xl:mt-28 lg:mt-28 sm:mt-8 mt-8 mx-auto
+                 
+                   px-5 gap-5"
+              >
+                <div className="login-title lg:w-2/4 font-sans text-3xl font-bold	px-2	 ">
+                  Kirish
+                </div>
+                <TextField
+                  label="Telefon raqam"
+                  // eslint-disable-next-line
+                  error={
+                    (values.phone && errors.phone) ||
+                    (values.phone === "" && errors.phone) ||
+                    errPhone
+                  }
+                  helperText={errors.phone || errPhone}
+                  variant="filled"
+                  size="small"
+                  name="phone"
+                  type="phone"
+                  onChange={(e) => {
+                    handleChange(e);
+                    setErrPhone(false);
+                    setErrPass(false);
+                  }}
+                  onBlur={handleBlur}
+                  value={values.phone}
+                />
+                <TextField
+                  // eslint-disable-next-line
+                  error={
+                    (values.password && errors.password) ||
+                    errors.password ||
+                    errPass
+                  }
+                  helperText={errors.password || errPass}
+                  label="Parol"
+                  variant="filled"
+                  size="small"
+                  name="password"
+                  onChange={(e) => {
+                    handleChange(e);
+                    setErrPhone(false);
+                    setErrPass(false);
+                  }}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  type="password"
+                />
+                <Button
+                  variant="contained"
+                  size="large"
+                  type="submit"
+                  disabled={submiting}
+                  style={{backgroundColor:'#D7144C'}}
+                >
+                  {submiting ? "Kutilmoqda..." : "Kirish"}
+                </Button>
+              </form>
+             
             </div>
           )}
         </Formik>
