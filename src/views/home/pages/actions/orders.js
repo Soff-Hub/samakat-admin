cbe9556c86de65020d89169e2ea85e61d408d23c
Farangiz17 <farangizismatova24@gmail.com>
2024-06-01 15:06:47 +0500

    finish order

--- src/views/home/pages/actions/orders.js
+++ src/views/home/pages/actions/orders.js
@@ -4,16 +4,17 @@ import { API_ENDPOINTS } from "service/ApiEndpoints";
 import Client from "service/Client";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
-import {
-  Button,
-  TextField,
-} from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useTheme } from "contexts/themeContex";
+import { Input } from "antd";
+const { TextArea } = Input;
 
 export default function Orders() {
   const locationn = useLocation();
   const [data, setData] = useState(null);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { theme } = useTheme();
 
   const getData = async (id) => {
     await Client.get(`${API_ENDPOINTS.DETAIL_ORDER}${id}/`)
@@ -27,22 +28,23 @@ export default function Orders() {
 
   const handleRouterAdress = (location, latitute, longitute) => {
     if (location) {
-      window.open(`https://www.google.com/maps/place/${location}/?q=${latitute},${longitute}`, '_blank')
+      window.open(
+        `https://www.google.com/maps/place/${location}/?q=${latitute},${longitute}`,
+        "_blank"
+      );
     }
-  }
+  };
 
   const handleChangeRouter = (id) => {
     if (id) {
-      navigate(`/users/actions/?detail?${id}`)
+      navigate(`/users/actions/?detail?${id}`);
     }
-  }
+  };
 
   useEffect(() => {
     getData(locationn.search.split("?")[1]);
     // eslint-disable-next-line
   }, []);
-
-
 
   return (
     <div className="px-2 py-3 bg--color">
@@ -51,7 +53,7 @@ export default function Orders() {
         <Link to="/orders">
           <Button
             variant="contained"
-            color="info"
+            color="primary"
             size="large"
             startIcon={<ArrowBackIcon />}
           >
@@ -62,47 +64,89 @@ export default function Orders() {
       <div>
         {data ? (
           <div
-            style={{ display: "flex", justifyContent: "center", alignItems:'center' }}
-            className="text-center w-full flex flex-col	"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            className="text-start w-full flex flex-col	"
           >
-            <form className="w-2/3 bg-slate-200 p-6  flex flex-col gap-5 mt-6  create-branch-form">
-              <TextField
-                label="Foydalanuvchi"
-                variant="outlined"
-                size="large"
-                type="text"
-                value={data?.user_about ? data?.user_about?.user : "-"}
-                onClick={() => handleChangeRouter(data.user_about?.id)}
-              />
-              <TextField
-                label="Umumiy so'mma"
-                variant="outlined"
-                size="large"
-                value={data.total_amount ? data?.total_amount  : "-"} 
-                type="text"
-              />
-              <TextField
-                label="Soni"
-                variant="outlined"
-                size="large"
-                value={data.count_products ? data?.count_products + " ta" : "-"}
-                type="text"
-              />
-              <TextField
-                label="Vaqti"
-                variant="outlined"
-                size="large"
-                value={data?.created_at ?  data?.created_at?.slice(0,10) + " | " +  data?.created_at?.slice(11,18) : "-"}
-                type="text"
-              />
-            <TextField
-                label="Manzil"
-                variant="outlined"
-                size="large"
-                value={data.address?.location ? data?.address?.location : "-"}
-                type="text"
-                onClick={() => handleRouterAdress(data?.address?.location ,data?.address?.latitude, data?.address?.longitude )}
-              />
+            <form
+              className={`w-2/3 bg-slate-200 p-6  flex flex-col gap-5 mt-6  create-branch-form ${
+                theme.palette.mode === "light"
+                  ? "colorr"
+                  : "colorr-b text-[#fff]"
+              } `}
+            >
+              <div>
+                <span className="p-1 font-medium text-sm text-start">
+                  Foydalanuvchi
+                </span>
+                <Input
+                  size="large"
+                  type="text"
+                  placeholder="Foydalanuvchi"
+                  value={data?.user_about ? data?.user_about?.user : "-"}
+                  onClick={() => handleChangeRouter(data.user_about?.id)}
+                />
+              </div>
+              <div>
+                <span className="p-1 font-medium text-sm text-start">
+                  Umumiy so'mma
+                </span>
+                <Input
+                  size="large"
+                  type="text"
+                  placeholder="Umumiy so'mma"
+                  value={data.total_amount ? data?.total_amount : "-"}
+                />
+              </div>
+              <div>
+                <span className="p-1 font-medium text-sm text-start">Soni</span>
+                <Input
+                  size="large"
+                  type="text"
+                  placeholder="Soni"
+                  value={
+                    data.count_products ? data?.count_products + " ta" : "-"
+                  }
+                />
+              </div>
+              <div>
+                <span className="p-1 font-medium text-sm text-start">
+                  Vaqti
+                </span>
+                <Input
+                  size="large"
+                  type="text"
+                  placeholder="Vaqti"
+                  value={
+                    data?.created_at
+                      ? data?.created_at?.slice(0, 10) +
+                        " | " +
+                        data?.created_at?.slice(11, 18)
+                      : "-"
+                  }
+                />
+              </div>
+              <div>
+                <span className="p-1 font-medium text-sm text-start">
+                  Manzil
+                </span>
+                <Input
+                  size="large"
+                  type="text"
+                  placeholder="Manzil"
+                  value={data.address?.location ? data?.address?.location : "-"}
+                  onClick={() =>
+                    handleRouterAdress(
+                      data?.address?.location,
+                      data?.address?.latitude,
+                      data?.address?.longitude
+                    )
+                  }
+                />
+              </div>
               {/* <TextField
                 label="Promo kod"
                 variant="outlined"
@@ -110,31 +154,34 @@ export default function Orders() {
                 value={data.promocode ? data?.promocode?.code : "-"}
                 type="text"
               /> */}
-              <TextField
-                label="Holat"
-                variant="outlined"
-                size="large"
-                value={
-                  data && data?.status === "approved"
-                    ? "tasdiqlangan"
-                    : data && data?.status === "process"
-                    ? "jarayonda"
-                    : data && data?.status === "cancelled"
-                    ? "bekor qilingan"
-                    : ""
-                }
-                type="text"
-              />
 
-              <TextField
-                label="Izoh"
-                variant="outlined"
-                size="large"
-                value={data.commentary ? data?.commentary : "-"}
-                type="text"
-                multiline
-                rows={4}
-              />
+              <div>
+                <span className="p-1 font-medium text-sm text-start">
+                  Holat
+                </span>
+                <Input
+                  size="large"
+                  type="text"
+                  placeholder="Holat"
+                  value={
+                    data && data?.status === "approved"
+                      ? "tasdiqlangan"
+                      : data && data?.status === "process"
+                      ? "jarayonda"
+                      : data && data?.status === "cancelled"
+                      ? "bekor qilingan"
+                      : ""
+                  }
+                />
+              </div>
+              <div>
+                <span className="p-1 font-medium text-sm text-start">
+                Izoh
+                </span>
+                <TextArea  value={data.commentary ? data?.commentary : "-"} placeholder="Izoh" allowClear rows={4} maxLength={6} />
+              </div>
+
+             
               {/* <div className="text-left flex align-center">
                 <label className="font-normal font-sans text-base pt-2 pr-2">
                   Eshik oldida qoldirish
@@ -151,31 +198,37 @@ export default function Orders() {
                 </RadioGroup>
               </div> */}
             </form>
-            {
-               data?.product_count?.length > 0 ?
-            <form className="w-2/3 mt-2  bg-slate-200 p-6 mt-6 ">
-              <p className="font-normal font-sans text-start text-lg pb-2" >Buyurtmalar:</p>
-              <ul className="border-[#AEB2B8] py-2 text-start border rounded">
-              {
-                  data?.product_count?.map((el,i) => (
-                    <li className="font-normal font-sans text-base pl-2 my-2" >
-                      <a style={{
-                        display:'flex',
-                        alignItems:'center',
-                        gap:'5px'
-                      }} 
-                       href={`/products/actions/?bistro?edit?${el.product_slug}`}>
-
-                     {i+1}. <img src={el.image} width={50} alt="alokand.uz" /> {el.product}
-
+            {data?.product_count?.length > 0 ? (
+              <form className={`w-2/3 mt-2  bg-slate-200 p-6 mt-6 ${
+                theme.palette.mode === "light"
+                  ? "colorr"
+                  : "colorr-b text-[#fff]"
+              } `}>
+                <p className="font-normal font-sans text-start text-lg pb-2">
+                  Buyurtmalar:
+                </p>
+                <ul className="border-[#AEB2B8] py-2 text-start border rounded">
+                  {data?.product_count?.map((el, i) => (
+                    <li className="font-normal font-sans text-base pl-2 my-2">
+                      <a
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "5px",
+                        }}
+                        href={`/products/actions/?bistro?edit?${el.product_slug}`}
+                      >
+                        {i + 1}.{" "}
+                        <img src={el.image} width={50} alt="alokand.uz" />{" "}
+                        {el.product}
                       </a>
                     </li>
-                  ))
-                }
-              </ul>
-            </form>
-              : " "
-            }
+                  ))}
+                </ul>
+              </form>
+            ) : (
+              " "
+            )}
           </div>
         ) : (
           <Box
